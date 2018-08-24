fc9437a85422a13abdb62b9ad62829fa9be347dc
EKSHIT NALWAYA <u1145430@umail.utah.edu>
2018-08-24 17:06:43 -0600

    First commit!

---
+++ Source/CorpseRevenge/CorpseRevenge.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "CorpseRevenge.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CorpseRevenge, "CorpseRevenge" );
+
+DEFINE_LOG_CATEGORY(LogCorpseRevenge)
+ 
---
+++ Source/CorpseRevenge/CorpseRevengeGameMode.cpp
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "CorpseRevengeGameMode.h"
+#include "CorpseRevengePlayerController.h"
+#include "CorpseRevengeCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ACorpseRevengeGameMode::ACorpseRevengeGameMode()
+{
+	// use our custom PlayerController class
+	PlayerControllerClass = ACorpseRevengePlayerController::StaticClass();
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
